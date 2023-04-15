85a9cfdb5facb643589cea1fdb58d0bb3c31b15e
Xurmatillo <96421607+Xurmatillo@users.noreply.github.com>
2023-04-15 17:08:58 +0500

    Update SeismoGuarg.ino

--- SeismoGuarg.ino
+++ SeismoGuarg.ino
@@ -1,83 +1,73 @@
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <WiFiManager.h>
-#include <ESP8266HTTPClient.h>
 
-// Set up pins for flashlight and speaker
-int flashlightPin = D3;
-int speakerPin = D5;
+#define SPEAKER_PIN D5
+#define FLASHLIGHT_PIN D3
+#define GPS_RX_PIN D7
+#define GPS_TX_PIN D8
+#define WIFI_SSID "your-wifi-ssid"
+#define WIFI_PASSWORD "your-wifi-password"
 
-// Set up variables for Wi-Fi network
-char ssid[30] = "myWifiNetwork";  // Change to your network name
-char password[30] = "myWifiPassword";  // Change to your network password
-boolean shouldSaveConfig = false;
-WiFiManager wifiManager;
+TinyGPSPlus gps;
+SoftwareSerial gpsSerial(GPS_RX_PIN, GPS_TX_PIN);
 
-// Set up web server for changing Wi-Fi password
 ESP8266WebServer server(80);
 
 void setup() {
-  // Set up pins for flashlight and speaker
-  pinMode(flashlightPin, OUTPUT);
-  pinMode(speakerPin, OUTPUT);
+  Serial.begin(115200);
+  gpsSerial.begin(9600);
 
-  // Connect to Wi-Fi network
-  wifiManager.setSaveConfigCallback(saveConfigCallback);
-  wifiManager.autoConnect("ESP8266AP");
-  if (shouldSaveConfig) {
-    saveConfig();
+  pinMode(SPEAKER_PIN, OUTPUT);
+  pinMode(FLASHLIGHT_PIN, OUTPUT);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
 
-  // Set up web server for changing Wi-Fi password
   server.on("/", handleRoot);
-  server.on("/submit", handleSubmit);
+  server.on("/wifi", handleWifi);
   server.begin();
-
-  // Start ShakeAlert connection
-  HTTPClient http;
-  http.begin("https://api.shakealert.org/alerts/");
-  http.addHeader("Content-Type", "application/json");
-  http.addHeader("Accept", "application/json");
-  int httpCode = http.GET();
-  if (httpCode > 0) {
-    String payload = http.getString();
-    // Parse earthquake data from ShakeAlert and trigger warning
-    // code here
-  }
-  http.end();
 }
 
 void loop() {
-  // Check if connected to Wi-Fi network
-  if (WiFi.status() == WL_CONNECTED) {
-    // Turn off broadcasting wifi
-    WiFi.mode(WIFI_STA);
-  } else {
-    // Turn on broadcasting wifi
-    WiFi.mode(WIFI_AP);
+  while (gpsSerial.available() > 0) {
+    if (gps.encode(gpsSerial.read())) {
+      Serial.print("Location: ");
+      Serial.print(gps.location.lat(), 6);
+      Serial.print(", ");
+      Serial.println(gps.location.lng(), 6);
+    }
   }
 
-  // Handle any incoming web server requests
-  server.handleClient();
+  // TODO: ShakeAlert integration code
 }
 
 void handleRoot() {
-  // Serve web page for changing Wi-Fi password
-  // code here
+  server.send(200, "text/html", "<html><body><h1>ESP8266 Earthquake Warning System</h1><p>Click <a href=\"/wifi\">here</a> to change Wi-Fi password.</p></body></html>");
 }
 
-void handleSubmit() {
-  // Save new Wi-Fi password and name
-  // code here
+void handleWifi() {
+  String html = "<html><body><h1>Change Wi-Fi Password</h1><form method=\"POST\" action=\"/update\"><label>SSID: <input type=\"text\" name=\"ssid\"></label><br><label>Password: <input type=\"password\" name=\"password\"></label><br><input type=\"submit\" value=\"Submit\"></form></body></html>";
+  server.send(200, "text/html", html);
 }
 
-void saveConfigCallback() {
-  shouldSaveConfig = true;
+void handleUpdate() {
+  String ssid = server.arg("ssid");
+  String password = server.arg("password");
+
+  // TODO: update Wi-Fi credentials
 }
 
-void saveConfig() {
-  wifiManager.setSTAStaticIPConfig(IPAddress(192, 168, 0, 99), IPAddress(192, 168, 0, 1), IPAddress(255, 255, 255, 0));
-  strcpy(ssid, wifiManager.getSSID().c_str());
-  strcpy(password, wifiManager.getPassword().c_str());
+void alert() {
+  digitalWrite(FLASHLIGHT_PIN, HIGH);
+  tone(SPEAKER_PIN, 1000, 1000);
+  delay(1000);
+  digitalWrite(FLASHLIGHT_PIN, LOW);
+  noTone(SPEAKER_PIN);
 }
+
